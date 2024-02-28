4dceb24f229c2559a6eeb14e241fb1293aa631c0
NuhDemir <115398614+NuhDemir@users.noreply.github.com>
2024-02-28 19:58:11 +0300

    Main.java

--- Find Number/src/Main.java
+++ Find Number/src/Main.java
@@ -1,17 +1,44 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Scanner nesnesi oluşturma
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Kullanıcıdan dizi boyutunu al
+        System.out.print("Dizinin boyutunu giriniz: ");
+        int size = scanner.nextInt();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Diziyi oluştur
+        int[] numbers = new int[size];
+
+        // Kullanıcıdan dizi elemanlarını al
+        for (int i = 0; i < size; i++) {
+            System.out.print("Sayı " + (i + 1) + " giriniz: ");
+            numbers[i] = scanner.nextInt();
         }
+
+        // Aranacak sayıyı al
+        System.out.print("Aranacak sayıyı giriniz: ");
+        int search_number = scanner.nextInt();
+
+        // Aranan sayının dizide olup olmadığını kontrol et
+        boolean isThere = false;
+        for (int number : numbers) {
+            if (number == search_number) {
+                isThere = true;
+                break;
+            }
+        }
+
+        // Sonucu ekrana yazdır
+        if (isThere) {
+            System.out.println("Sayı Mevcuttur");
+        } else {
+            System.out.println("Sayı Mevcut Değildir");
+        }
+
+        // Scanner nesnesini kapat
+        scanner.close();
     }
 }
